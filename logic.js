0735d3047c50c0151ccec20342a3fec94bde1af8
mairishavhoon <rishav1122@gmail.com>
2021-07-12 21:14:29 +0530

    added backend logic

---
+++ logic.js
@@ -0,0 +1,12 @@
+
+function beast(){
+    var beas = document.getElementById("beast")
+    beas.play();
+}
+
+function bird(){
+    var beas = document.getElementById("bird")
+    beas.play();
+}
+
+
