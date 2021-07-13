44c02814c6d3beb11eab3fd14ed7bd5436818206
mairishavhoon <rishav1122@gmail.com>
2021-07-13 17:58:58 +0530

    added frontend

--- logic.js
+++ logic.js
@@ -1,6 +1,6 @@
 
-function beast(){
-    var beas = document.getElementById("beast")
+function monke(){
+    var beas = document.getElementById("monke")
     beas.play();
 }
 
@@ -9,4 +9,32 @@ function bird(){
     beas.play();
 }
 
+function insect(){
+    var beas = document.getElementById("insect")
+    beas.play();
+}
 
+function lion(){
+    var beas = document.getElementById("lion")
+    beas.play();
+}
+
+function cat(){
+    var beas = document.getElementById("cat")
+    beas.play();
+}
+
+function dog(){
+    var beas = document.getElementById("dog")
+    beas.play();
+}
+
+function cow(){
+    var beas = document.getElementById("cow")
+    beas.play();
+}
+
+function horse(){
+    var beas = document.getElementById("horse")
+    beas.play();
+}
